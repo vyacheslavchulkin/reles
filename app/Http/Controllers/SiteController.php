534fc6accbac09335d8c1c7e065a531e79efc47c
Viacheslav Ostrovskiy <chelout@gmail.com>
2020-12-22 23:39:16 +0300

    Добавил регистрацию/авторизацию учеников

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -2,11 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class SiteController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         return view('site.index');
     }
 }
