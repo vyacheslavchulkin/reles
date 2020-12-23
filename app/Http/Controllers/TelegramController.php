cbd4513a21c4bde74750ab6d1c615936501da50c
Vyacheslav Chulkin <v.n.chulkin@gmail.com>
2020-12-23 19:31:59 +0300

    Добавлина обработка исключений

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -5,12 +5,18 @@ namespace App\Http\Controllers;
 
 
 use App\Bots\Telegram\TelegramBot;
+use Exception;
+use Illuminate\Support\Facades\Log;
 
 class TelegramController
 {
     public function webhook()
     {
-        $bot = new TelegramBot();
-        $bot->run();
+        try {
+            $bot = new TelegramBot();
+            $bot->run();
+        } catch (Exception $e) {
+            Log::error($e->getMessage());
+        }
     }
 }
